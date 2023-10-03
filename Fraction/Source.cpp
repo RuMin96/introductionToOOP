f00a56fab2bbbbb5ad3e066eb4a8163b6001cfb5
PC <PC@DESKTOP-OR1E14N>
2023-10-04 00:26:27 +0700

    DZZ

--- Fraction/Source.cpp
+++ Fraction/Source.cpp
@@ -1,110 +1,35 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
 class Fraction
 {
 	int integer;
-	int numenator;
-	int denomenator;
+	int numerator;
+	int denominator;
 public:
-	int get_integer()const
+	Fraction(int integ, int num, int denom)
+		:integer{ integ }, numerator{ num }, denominator{ denom }
 	{
-		return integer;
+		cout << "Fraction constructor" << this << endl;
 	}
-	int get_numenator()const
-	{
-		return numenator;
-	}
-	int get_denomenator()const
-	{
-		return denomenator;
-	}
-	void set_integer(int integer)
-	{
-		this->integer = integer;
-	}
-	void set_numenator(int numenator)
-	{
-		this->numenator = numenator;
-	}
-	void set_denomenator(int denomenator)
-	{
-		if (denomenator == 0)denomenator = 1;
-		this->denomenator = denomenator;
-	}
-	Fraction()
-	{
-		this->integer = 0;
-		this->numenator = 0;
-		this->denomenator = 1;
-		cout << "DefaultContructor:/t" << this << endl;
-	}
-	Fraction(int integer)
-	{
-		this->integer = integer;
-		this->numenator = 0;
-		this->denomenator = 1;
-	}
-	Fraction(int numenator, int denomenator)
-	{
-		this->integer = 0;
-		this->numenator = numenator;
-		set_denomenator(denomenator);
-	}
-	Fraction(int integer, int numenator, int denomenator)
-	{
-		this->integer = integer;
-		this->numenator = numenator;
-		set_denomenator(denomenator);
-		cout << "Constructor:\t" << this << endl;
-	}
-	Fraction(const Fraction& other)
-	{
-		this->integer = other.integer;
-		this->numenator = other.numenator;
-		this->denomenator = other.denomenator;
-		cout << "CopyConstructor:" << this << endl;
-	}
+	Fraction():Fraction(5,1,1){}
 	~Fraction()
 	{
-		cout << "Destructor:\t" << this << endl;
+		cout << "Fraction destructor" << this << endl;
 	}
-
-
-void print()const
-{
-	if(integer)cout << integer;
-	if (numenator)
+	void print()
 	{
-		if (integer)cout << "(";
-		cout << numenator << "/" << denomenator;
-		if (integer)cout << ")";
+		cout << integer << "(" << numerator << " / " << denominator << ")" << endl;
 	}
-	else if (integer == 0)cout << 0;
-	cout << endl;
-
-}
 };
-
-//#define CONSTRUCTORS_CHECK
-
-void main()
+int main()
 {
-	setlocale(LC_ALL, "");
-#ifdef CONSTRUCTORS_CHECK
-	Fraction A;
-	A.print();
-
-	Fraction B = 5;
-	B.print();
-
-	Fraction C(1, 2);
-	C.print();
-
-	Fraction D(2, 3, 4);
-	D.print();
-
-	Fraction E = C;
-	E.print();
-#endif CONSTRUCTORS_CHECK
+	Fraction a{ 6,5,8 };
+	Fraction b{ a };
+	cout << "a = ";
+	a.print();
+	cout << "\nb = ";
+    b.print();
+	cout << '\n';
+	return 0;
 }
--- introductionToOOP/Source.cpp
+++ introductionToOOP/Source.cpp
@@ -1,6 +1,8 @@
 #include<iostream>
 using namespace std;
 
+#define delimiter "\n-------------------------------------------------------------------\n"
+
 class Point
 {
 	double x;
@@ -23,24 +25,109 @@ public:
 	{
 		this->y = y;
 	}
-	double distance(Point other)
+	/*Point()
+	{
+		x = y = double();
+		cout << "DefaultConstructor:\t" << this << endl;
+	}
+	Point(double x)
+	{
+		this->x = x;
+		this->y = 0;
+		cout << "1Argument constructor:\t" << this << endl;
+	}*/
+	Point(double x=0, double y=0)
+	{
+		this->x = x;
+		this->y = y;
+		cout << "Constructor:\t\t" << this << endl;
+	}
+	Point(const Point& other)
+	{
+		this->x = other.x;
+		this->y = other.y;
+		cout << "CopyConstructor:\t" << this << endl;
+	}
+	~Point()
+	{
+		cout << "Destructor:\t\t" << this << endl;
+	}
+
+	Point& operator=(const Point& other)
+	{
+		this->x = other.x;
+		this->y = other.y;
+		cout << "CopyAssigment:\t\t" << this << endl;
+		return *this;
+	}
+	Point& operator+=(const Point& other)
+	{
+		this->x += other.x;
+		this->y += other.y;
+		return *this;
+	}
+
+	Point& operator++()
+	{
+		x++;
+		y++;
+		return *this;
+	}
+	Point operator++(int)
+	{
+		Point old = *this;
+		x++;
+		y++;
+		return old;
+	}
+
+	double distance(Point& other) const
 	{
 		double x_distance = this->x - other.x;
 		double y_distance = this->y - other.y;
 		double distance = sqrt(x_distance * x_distance + y_distance * y_distance);
 		return distance;
 	}
+	void print()
+	{
+		cout << "X=" << x << "\tY=" << y << endl;
+	}
 };
 
-double distance(Point A, Point B)
+	ostream& operator<<(ostream& os, const Point& obj)
+	{
+		return os << "X=" << obj.get_x() << "\tY=" << obj.get_y();
+	}
+double distance(Point& A, Point& B)
 {
 	double x_distance = A.get_x() - B.get_x();
 	double y_distance = A.get_y() - B.get_y();
 	return sqrt(x_distance * x_distance + y_distance * y_distance);
+}
 
+Point operator+(const Point& left, const Point& right)
+{
+	/*Point result;
+	result.set_x(left.get_x() + right.get_x());
+	result.set_y(left.get_y() + right.get_y());
+	return result;*/
+	/*Point result
+	(
+	left.get_x() + right.get_x(),
+	left.get_y() + right.get_y()
+	);
+	return result;*/
+	return Point
+	(
+		left.get_x() + right.get_x(),
+		left.get_y() + right.get_y()
+	);
 }
 //#define STRUCT_POINT
-#define DISTANCE_CHECK
+//#define DISTANCE_CHECK
+//#define CONSTRUCTORS_CHECK
+//#define ASSIGMENT_CHECK
+//#define INCREMENT_CHECK
 
 void main()
 {
@@ -68,12 +155,18 @@ void main()
 	B.set_x(7);
 	B.set_y(8);
 	cout << "B:\t" << B.get_x() << "\t" << B.get_y() << endl;
+	cout << delimiter << endl;
 	cout << "Расстояние от точки 'A' до точки 'B' : \t " << A.distance(B) << endl;
+	cout << delimiter << endl;
 	cout << "Расстояние от точки 'B' до точки 'A' : \t " << B.distance(A) << endl;
+	cout << delimiter << endl;
 	cout << "Расстояние между точками 'A' и 'B':\t" << distance(A, B) << endl;
+	cout << delimiter << endl;
 	cout << "Расстояние между точками 'B' и 'A':\t" << distance(B, A) << endl;
+	cout << delimiter << endl;
 	
 #endif DISTANCE_CHECK
+#ifdef INCREMENT_CHECK
 	int a = 2;
 	int b = 3;
 	int c = a + b;
@@ -83,6 +176,64 @@ void main()
 	Point B(7, 8);
 	Point C = A + B;
 	C.print();
+
+	A=B++;
+	A.print();
+	B.print();
+#endif INCREMENT_CHECK
+
+
+	/*for (int i = 0; i < 10; i++)
+	{
+		cout << i << "\t";
+	}
+	cout << endl;*/
+#ifdef CONSTRUCTORS_CHECK
+	cout << int() << endl;
+	Point A;
+	A.print();
+
+	Point B = 5;
+	B.print();
+
+	Point C(2, 3);
+	C.print();
+
+	Point D = C;
+	D.print();
+
+	Point E;
+	E = D;
+	E.print();
+
+
+#endif 
+
+#ifdef ASSIGMENT_CHECK
+	int a, b, c;
+	a = b = c = 0;
+	cout << a << "\t" << b << "\t" << c << endl;
+
+	Point A, B, C;
+
+	A = B = C = Point(2, 3);
+
+	A.print();
+	B.print();
+	C.print();
+#endif ASSIGMENT_CHECK
+
+
+	Point A(2, 3);
+	A.print();
+	Point B(7, 8);
+	B.print();
+	
+	A += B;
+	//A.print();
+
+	cout << A << endl;
+
 }
 
 /*
